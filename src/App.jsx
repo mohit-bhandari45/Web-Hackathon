cf2b8bb3449088cfa5acb3457a398c9adc0f24b1
mohit-bhandari45 <mohitbhandari852@gmail.com>
2024-04-06 22:58:57 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Universal/Navbar.jsx'
+import HeroSec from "./components/HomeComp/HeroSec.jsx"
+import Head from './components/Universal/Head.jsx'
+import SubHero1 from './components/HomeComp/SubHero1.jsx'
+import SubHero2 from './components/HomeComp/SubHero2.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Navbar/>
+    <HeroSec/>
+    <Head title="Why Able Innovation Exists"/>
+    <SubHero1/>
+    <Head title="Our Innovation"/>
+    <SubHero2/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter,Route, RouterProvider } from 'react-router-dom'
+import About from "./pages/About.jsx"
+import Contact from "./pages/Contact.jsx"
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>
+  },
+  {
+    path:"/",
+    element:<About/>
+  },
+  {
+    path:"/",
+    element:<Contact/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}></RouterProvider>
 )
